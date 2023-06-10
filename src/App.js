8d2042d4b3d874d29092a891dc451daf9c14ca0a
asfund <asfundmirzaj@gmail.com>
2023-06-10 19:53:10 +0500

    fixed height of sidebar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Login from "./components/Login/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./components/Register/Register";
 import LandingPage from "./views/LandingPage/landingPage";
+import Header from "./components/Dadshboard/Header";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <Header />
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<LandingPage />} />
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/register" element={<Register />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
